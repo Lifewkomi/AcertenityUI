b99ca1e046b5bbc600399d61cb305e16661b796d
Philemon Komi <philemonkomi46@gmail.com>
2024-08-03 16:51:15 +0300

    Fixed 3dCardEffect and LayoutGrid for React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import NavbarDemo from "./components/FloatingNavbar/Demo"
+import LayoutGridDemo from "./components/LayoutGrid/Demo"
+import ThreeCardDemo from "./components/3dCardEffect/Demo";
+
+export default function App() {
+  return (
+    <div className="bg-black" >
+      {/* <NavbarDemo /> */}
+      {/* <LayoutGridDemo /> */}
+      <ThreeCardDemo />
+    </div>
+  )
+}
